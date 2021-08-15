3a9520b566a4111d41f4f9ece4a65b21b3034810
Vxty <77857480+SlayerM7@users.noreply.github.com>
2021-08-15 07:38:07 +0200

    Add files via upload

---
+++ src/index.ts
@@ -0,0 +1,77 @@
+import db from "./database/db";
+import readline from "readline";
+import { createString } from "./functions/utils/createString";
+import { logo, startMenuLogo } from "./prints/logo";
+import login from "./functions/start-menu/login";
+import register from "./functions/start-menu/register";
+import child_process from "child_process";
+import request from "request";
+import open from "open";
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function toolStart() {
+  // child_process.exec("mode 85,20");
+  console.clear();
+  request(
+    {
+      url: "https://external-nuker-api-2.0w218021.repl.co/update",
+      method: "GET",
+      json: {
+        Version: 3.2,
+      },
+    },
+    (err, res, body) => {
+      if (res.body.status === true) {
+        logo();
+        console.log(" ");
+        console.log(
+          createString("You are on a out-dated version", "semi", "fail")
+        );
+        console.log(" ");
+        rl.question(createString("Type anything to update"), () => {
+          open(res.body.message);
+          console.log(" ");
+          console.log(createString("Closing window in 5 seconds", "semi"));
+          setTimeout(() => {
+            process.exit();
+          }, 5000);
+        });
+        return;
+      }
+      logo();
+      startMenuLogo();
+      rl.question(createString("Enter option"), (option) => {
+        if (option === "1") {
+          login(rl);
+        } else if (option === "2") {
+          register(rl);
+        } else if (option === "4") {
+          if (res.body.status === true) {
+            open(res.body.message);
+          } else {
+            console.log("");
+            console.log(createString("You are on the newest version!", "semi"));
+            setTimeout(() => {
+              toolStart();
+            }, 1500);
+            return;
+          }
+        } else {
+          console.log("");
+          console.log(createString("COMING SOON!", "semi", "fail"));
+          setTimeout(() => {
+            toolStart();
+          }, 1500);
+          return;
+        }
+      });
+    }
+  );
+}
+toolStart();
+
+export default toolStart;
